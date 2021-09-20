434a8ce39bd0d47e5212ee8f9fa47308a1da294b
Alexei Catana <alexei.catana.bras@gmail.com>
2021-09-20 23:08:21 +0300

    Tema6 - metoda calculatePrice

---
+++ src/main/java/ro/sci/Tema6/src/Calculateprice.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Calculateprice {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the price without Vat");
+        double basePrice = scanner.nextInt();
+
+        System.out.println("Please enter the amount of Vat in procentage (%)");
+        double vat = scanner.nextInt();
+
+        calculatePrice(basePrice, vat);
+    }
+
+    public static void calculatePrice(double basePrice, double vat) {
+        System.out.println("Your total price is " + (basePrice * ((100 + vat) / 100)));
+    }
+
+}
