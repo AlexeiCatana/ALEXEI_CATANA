9da8ed122d8ca65f49dbc03191069418becde51e
Alexei Catana <alexei.catana.bras@gmail.com>
2021-09-20 23:43:22 +0300

    Tema3 - Greetings

---
+++ src/main/java/ro/sci/Tema03/src/main/java/Greetings.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Greetings {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please type in your name");
+        String name = scanner.nextLine();
+
+        if (name.equals("")) {
+            System.out.println("Hello " + "stranger");
+        } else {
+            System.out.println("Hello " + name);
+        }
+    }
+}
